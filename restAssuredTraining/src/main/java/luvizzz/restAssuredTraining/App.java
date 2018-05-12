82d4e4ba8129444ebf0b462da4b4d47daccf3429
luvizzz <pedro.luvizotto@gmail.com>
2018-05-12 11:56:57 -0300

    Code Base with restAssured dependency

---
+++ restAssuredTraining/src/main/java/luvizzz/restAssuredTraining/App.java
@@ -0,0 +1,13 @@
+package luvizzz.restAssuredTraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
